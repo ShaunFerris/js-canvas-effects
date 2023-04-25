9b1056a1998e36505db089f33bafb5db92324873
Shaun Ferris <slb.ferris46@gmail.com>
2023-04-25 19:24:33 +0200

    Initial commit: boiler plate code

---
+++ main.js
@@ -0,0 +1,28 @@
+// Get the canvas element from the HTML file
+const canvas = document.getElementById('myCanvas');
+
+// Set the canvas dimensions and get the 2D drawing context
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+// Define any variables or constants needed for your animations
+let x = 0;
+let y = 0;
+
+// Define functions for drawing and animating your graphics
+function draw() {
+	// Add code here to draw your graphics on the canvas
+	ctx.fillRect(x, y, 50, 50);
+}
+
+function animate() {
+	// Add code here to update the position of your graphics and trigger new frames
+	x++;
+	y++;
+	requestAnimationFrame(animate);
+}
+
+// Call your animation functions to start the animation loop
+draw();
+animate();
