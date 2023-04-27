e626a099b9a628b0daa6d6c4514023b53dfca00b
Shaun Ferris <slb.ferris46@gmail.com>
2023-04-27 14:56:24 +0200

    Setup canvas function and start animation class

--- lightning/main.js
+++ lightning/main.js
@@ -2,4 +2,28 @@
 Implementation of an animated lightning background for
 integration into web projects.
 */
+function setupCanvas() {
+    /*
+    Get the canvas element from html, save the 2d animation context
+    and the terminal dimensions.
+    Returns the context, terminalx and terminaly as an object keyed
+    with the variable names.
+    */
+    const canvas = document.getElementById('myCanvas');
+    const c = canvas.getContext('2d');
+    let termx = window.innerWidth;
+    let termy = window.innerHeight;
+    canvas.width = termx;
+    canvas.height = termy;
+    return {c, termx, termy}
+}
 
+class canvasLightning {
+    /*
+    Class to contain animation functions and properties. Initialized
+    with the canvas properties from setupCanvas().
+    */
+    constructor(c, termx, termy) {
+
+    }
+}
